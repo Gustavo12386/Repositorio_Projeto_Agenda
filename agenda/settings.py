ec4311cad31f717835a78eb6fb4da83941c96b54
Gustavo Viana <81700849+Gustavo12386@users.noreply.github.com>
2022-09-01 22:45:54 -0300

    atualizacao

--- agenda/settings.py
+++ agenda/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_mysql',
+        'USER':'gustavo12386',
+        'PASSWORD': '123456789',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
